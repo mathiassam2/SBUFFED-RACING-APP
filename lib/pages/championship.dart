9798338cd7ea6799d7b8bd915a892d94a7545145
mathiassam2 <mathiassam2@gmail.com>
2024-08-19 21:47:58 +0800

    added julian, matrix, isaac png

--- lib/pages/championship.dart
+++ lib/pages/championship.dart
@@ -16,6 +16,7 @@ class ChampionshipPage extends StatelessWidget {
             lastName: 'Sam',
             pos: '01',
             pts: '40',
+            imagePath: 'assets/images/julian_nelson.png', // Path to image
           ),
           SizedBox(height: 8.0), // Add space between cards
           CustomCard(
@@ -23,6 +24,7 @@ class ChampionshipPage extends StatelessWidget {
             lastName: 'Verstappen',
             pos: '02',
             pts: '38',
+            imagePath: 'assets/images/isaac_anderson.png', // Path to image
           ),
           SizedBox(height: 8.0), // Add space between cards
           CustomCard(
@@ -30,6 +32,7 @@ class ChampionshipPage extends StatelessWidget {
             lastName: 'Hamilton',
             pos: '03',
             pts: '36',
+            imagePath: 'assets/images/matrix_saul.png', // Path to image
           ),
         ],
       ),
@@ -57,11 +60,13 @@ class ChampionshipPage extends StatelessWidget {
   }
 }
 
+
 class CustomCard extends StatelessWidget {
   final String firstName;
   final String lastName;
   final String pos;
   final String pts;
+  final String imagePath;
 
   const CustomCard({
     super.key,
@@ -69,6 +74,7 @@ class CustomCard extends StatelessWidget {
     required this.lastName,
     required this.pos,
     required this.pts,
+    required this.imagePath,
   });
 
   @override
@@ -76,80 +82,109 @@ class CustomCard extends StatelessWidget {
     return Card(
       color: const Color.fromARGB(255, 29, 29, 29),
       child: SizedBox(
-        height: 245.0, // Set the height of the card
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                firstName,
-                style: const TextStyle(
-                  fontSize: 16,
-                  color: Colors.white70,
-                ),
-              ),
-              Text(
-                lastName,
-                style: const TextStyle(
-                  fontSize: 32,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                ),
-              ),
-              const Spacer(), // Pushes content to the top and bottom
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
+        height: 245.0,
+        child: Stack(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 16.0, top: 14.0, right: 16.0, bottom: 5.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const Text(
-                        'Pos',
-                        style: TextStyle(
-                          fontSize: 14,
-                          color: Colors.white70,
-                        ),
-                      ),
-                      Text(
-                        pos,
-                        style: const TextStyle(
-                          fontSize: 36,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
-                        ),
-                      ),
-                    ],
+                  Text(
+                    firstName,
+                    style: const TextStyle(
+                      fontSize: 16,
+                      color: Colors.white70,
+                    ),
                   ),
-                  const SizedBox(width: 25.0), // Tab space equivalent
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
+                  Text(
+                    lastName,
+                    style: const TextStyle(
+                      fontSize: 32,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                  const Spacer(),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
                     children: [
-                      const Text(
-                        'Pts',
-                        style: TextStyle(
-                          fontSize: 14,
-                          color: Colors.white70,
-                        ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text(
+                            'Pos',
+                            style: TextStyle(
+                              fontSize: 14,
+                              color: Colors.white70,
+                            ),
+                          ),
+                          Text(
+                            pos,
+                            style: const TextStyle(
+                              fontSize: 36,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
                       ),
-                      Text(
-                        pts,
-                        style: const TextStyle(
-                          fontSize: 36,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
-                        ),
+                      const SizedBox(width: 25.0),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text(
+                            'Pts',
+                            style: TextStyle(
+                              fontSize: 14,
+                              color: Colors.white70,
+                            ),
+                          ),
+                          Text(
+                            pts,
+                            style: const TextStyle(
+                              fontSize: 36,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
                       ),
                     ],
                   ),
                 ],
               ),
-            ],
-          ),
+            ),
+            Positioned(
+              right: -190,
+              bottom: -320,
+              child: Container(
+                width: 550, // Width of the image
+                height: 550, // Height of the image
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.transparent, // Red border color
+                    width: 2, // Width of the border
+                  ),
+                ),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(8.0),
+                  child: Image.asset(
+                    imagePath,
+                    width: 100,
+                    height: 100,
+                    fit: BoxFit.fitHeight,
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
   }
 }
+
+
 
 
