c3b8393ef79e0d1a4bfa74c6920a61a4ef94b773
Mathias Sam <mathiassam2@gmail.com>
2024-08-20 12:17:17 +0800

    add team image to card in championship.dart

--- lib/pages/championship.dart
+++ lib/pages/championship.dart
@@ -18,6 +18,7 @@ class ChampionshipPage extends StatelessWidget {
             pts: '40',
             driverImagePath: 'assets/images/drivers/julian_nelson.png',
             teamColor: '#F7DA1B',
+            teamImagePath: 'assets/images/constructors/renault.png',
           ),
           SizedBox(height: 8.0),
           DriverCard(
@@ -27,6 +28,7 @@ class ChampionshipPage extends StatelessWidget {
             pts: '38',
             driverImagePath: 'assets/images/drivers/isaac_anderson.png',
             teamColor: '#08B1A0',
+            teamImagePath: 'assets/images/constructors/mercedes.png',
           ),
           SizedBox(height: 8.0),
           DriverCard(
@@ -36,6 +38,7 @@ class ChampionshipPage extends StatelessWidget {
             pts: '36',
             driverImagePath: 'assets/images/drivers/matrix_saul.png',
             teamColor: '#FE762A',
+            teamImagePath: 'assets/images/constructors/mclaren.png',
           ),
         ],
       ),
@@ -70,6 +73,7 @@ class DriverCard extends StatelessWidget {
   final String pts;
   final String driverImagePath;
   final String teamColor;
+  final String teamImagePath; // New parameter for team logo
 
   const DriverCard({
     super.key,
@@ -79,6 +83,7 @@ class DriverCard extends StatelessWidget {
     required this.pts,
     required this.driverImagePath,
     required this.teamColor,
+    required this.teamImagePath, // Initialize the team logo
   });
 
   @override
@@ -119,82 +124,13 @@ class DriverCard extends StatelessWidget {
                 ),
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.only(left: 16.0, top: 14.0, right: 16.0, bottom: 5.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    firstName,
-                    style: const TextStyle(
-                      fontSize: 16,
-                      color: Colors.white,
-                    ),
-                  ),
-                  Text(
-                    lastName,
-                    style: TextStyle(
-                      fontSize: 32,
-                      fontWeight: FontWeight.bold,
-                      color: customColor, // Use custom color for last name
-                    ),
-                  ),
-                  const Spacer(),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          const Text(
-                            'Pos',
-                            style: TextStyle(
-                              fontSize: 14,
-                              color: Colors.white70,
-                            ),
-                          ),
-                          Text(
-                            pos,
-                            style: const TextStyle(
-                              fontSize: 36,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white,
-                            ),
-                          ),
-                        ],
-                      ),
-                      const SizedBox(width: 25.0),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          const Text(
-                            'Pts',
-                            style: TextStyle(
-                              fontSize: 14,
-                              color: Colors.white70,
-                            ),
-                          ),
-                          Text(
-                            pts,
-                            style: const TextStyle(
-                              fontSize: 36,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
+            // Driver image (in the background, should be below name and team logo)
             Positioned(
-              right: 0,
               bottom: 0,
+              right: 0,
               child: Container(
-                width: 220, // Width of the image
-                height: 220, // Height of the image
+                width: 210, // Width of the image
+                height: 210, // Height of the image
                 decoration: BoxDecoration(
                   border: Border.all(
                     color: Colors.transparent, // Transparent border color
@@ -212,6 +148,104 @@ class DriverCard extends StatelessWidget {
                 ),
               ),
             ),
+            // Name and team logo (in the foreground, should be above gradient and driver image)
+            Padding(
+              padding: const EdgeInsets.only(left: 16.0, top: 14.0, right: 16.0, bottom: 5.0),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  // Team logo image with red border and top margin
+                  Container(
+                    margin: const EdgeInsets.only(top: 5.0), // Top margin
+                    width: 50, // Adjust width as needed
+                    height: 50, // Adjust height as needed
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(4.0), // Adjust radius to match image
+                      child: Image.asset(
+                        teamImagePath,
+                        width: 50,
+                        height: 50,
+                        fit: BoxFit.contain,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(width: 16.0), // Space between image and text
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        firstName,
+                        style: const TextStyle(
+                          fontSize: 16,
+                          color: Colors.white,
+                        ),
+                      ),
+                      Text(
+                        lastName,
+                        style: TextStyle(
+                          fontSize: 28,
+                          fontWeight: FontWeight.bold,
+                          color: customColor, // Use custom color for last name
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            // Positioning the position and points info (in the foreground, should be above other elements)
+            Positioned(
+              left: 0,
+              bottom: 0,
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Row(
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text(
+                          'Pos',
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: Colors.white70,
+                          ),
+                        ),
+                        Text(
+                          pos,
+                          style: const TextStyle(
+                            fontSize: 36,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(width: 25.0),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text(
+                          'Pts',
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: Colors.white70,
+                          ),
+                        ),
+                        Text(
+                          pts,
+                          style: const TextStyle(
+                            fontSize: 36,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
           ],
         ),
       ),
@@ -224,6 +258,9 @@ class DriverCard extends StatelessWidget {
     return Color(int.parse("FF$teamColor", radix: 16));
   }
 }
+
+
+
 
 
 
