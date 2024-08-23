b134eb516732ea7e23fb41ca2f9510fdc99c9b23
Mathias Sam <mathiassam2@gmail.com>
2024-08-23 16:21:04 +0800

    add const

--- lib/pages/auth_page.dart
+++ lib/pages/auth_page.dart
@@ -19,7 +19,7 @@ class AuthPage extends StatelessWidget {
           }
           // user is NOT logged in
           else {
-            return LoginPage();
+            return const LoginPage();
           }
         },
       ),
