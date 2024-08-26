29c3916b32b51b23cabbd3a356a0cfa8ef9ebe99
Mathias Sam <mathiassam2@gmail.com>
2024-08-26 11:39:07 +0800

    refactor and redesign login_page.dart

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -9,11 +9,18 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-  final emailController = TextEditingController();
-  final passwordController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
 
   bool _hasError = false;
   String _errorMessage = '';
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
 
   void loginUser() async {
     // Show loading circle
@@ -60,108 +67,207 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      body: Center(
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 24.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                // App Logo
-                Center(
-                  child: Image.asset(
-                    'assets/images/app_logo/sbuffed_logo.png',
-                    height: 100.0, // Adjust the height as needed
-                    width: 100.0,  // Adjust the width as needed
-                  ),
+      body: GestureDetector(
+        onTap: () {
+          // Unfocus all input fields when tapping outside
+          FocusScope.of(context).unfocus();
+        },
+        child: Column(
+          children: [
+            Expanded(
+              child: SingleChildScrollView(
+                padding: const EdgeInsets.symmetric(horizontal: 24.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    const SizedBox(height: 100.0), // Padding from top
+                    _buildLogo(),
+                    const SizedBox(height: 25.0), // Space between logo and form
+                    _buildErrorMessage(),
+                    const SizedBox(height: 16.0),
+                    _buildGoogleSignInButton(), // Add Google sign-in button
+                    _buildOrSeparator(), // Add OR separator
+                    _buildEmailTextField(),
+                    const SizedBox(height: 10.0),
+                    _buildPasswordTextField(),
+                  ],
                 ),
-                const SizedBox(height: 25.0), // Add space between logo and form
-
-                // Error Message Slide Animation
-                AnimatedSlide(
-                  offset: _hasError ? Offset.zero : const Offset(0, -1),
-                  duration: const Duration(milliseconds: 300),
-                  child: Text(
-                    _errorMessage,
-                    textAlign: TextAlign.center,
-                    style: const TextStyle(
-                      color: Colors.red,
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 16.0),
-
-                // Email TextField
-                TextField(
-                  controller: emailController,
-                  style: const TextStyle(color: Colors.white),
-                  decoration: InputDecoration(
-                    hintText: 'Email',
-                    hintStyle: const TextStyle(color: Colors.grey),
-                    filled: true,
-                    fillColor: Colors.white10,
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(12),
-                      borderSide: _hasError
-                          ? const BorderSide(color: Colors.red, width: 2.0)
-                          : const BorderSide(color: Colors.white38, width: 1.5),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(12),
-                      borderSide: const BorderSide(color: Color(0xFFF7DA1B), width: 2.0),
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 16.0),
-
-                // Password TextField
-                TextField(
-                  controller: passwordController,
-                  style: const TextStyle(color: Colors.white),
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    hintText: 'Password',
-                    hintStyle: const TextStyle(color: Colors.grey),
-                    filled: true,
-                    fillColor: Colors.white10,
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(12),
-                      borderSide: _hasError
-                          ? const BorderSide(color: Colors.red, width: 2.0)
-                          : const BorderSide(color: Colors.white38, width: 1.5),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(12),
-                      borderSide: const BorderSide(color: Color(0xFFF7DA1B), width: 2.0),
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 32.0),
-
-                // Log In Button
-                ElevatedButton(
-                  onPressed: loginUser,
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: const Color(0xFFF7DA1B),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(12),
-                    ),
-                    padding: const EdgeInsets.symmetric(vertical: 16.0),
-                  ),
-                  child: const Text(
-                    'Log In',
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-              ],
+              ),
             ),
+            Padding(
+              padding: const EdgeInsets.all(24.0), // Adjust padding as needed
+              child: _buildLoginButton(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+
+  Widget _buildOrSeparator() {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 19.0), // Top and bottom margin
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Expanded(
+            child: Container(
+              margin: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: const Divider(
+                color: Color(0xFF30303D),
+                thickness: 2.0,
+              ),
+            ),
+          ),
+          const Text(
+            'OR',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 14,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          Expanded(
+            child: Container(
+              margin: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: const Divider(
+                color: Color(0xFF30303D),
+                thickness: 2.0,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildGoogleSignInButton() {
+    return ElevatedButton(
+      onPressed: () {
+        // Implement Google sign-in functionality here
+      },
+      style: ElevatedButton.styleFrom(
+        backgroundColor: Colors.transparent, // Transparent background
+        side: const BorderSide(color: Color(0xFF30303D), width: 2.0), // Border color and size
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(7),
+        ),
+        padding: const EdgeInsets.symmetric(vertical: 16.0),
+        foregroundColor: Colors.white, // Text color
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset(
+            'assets/images/app_logo/google_logo_white.png', // Path to your Google logo asset
+            height: 24.0,
+            width: 24.0,
+          ),
+          const SizedBox(width: 8.0),
+          const Text(
+            'Continue with Google',
+            style: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+              color: Colors.white, // Text color
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildLogo() {
+    return Center(
+      child: Image.asset(
+        'assets/images/app_logo/sbuffed_text_logo.png',
+        height: 100.0,
+        width: 300.0,
+      ),
+    );
+  }
+
+  Widget _buildErrorMessage() {
+    return AnimatedSlide(
+      offset: _hasError ? Offset.zero : const Offset(0, -1),
+      duration: const Duration(milliseconds: 300),
+      child: Text(
+        _errorMessage,
+        textAlign: TextAlign.center,
+        style: const TextStyle(
+          color: Colors.red,
+          fontSize: 16,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    );
+  }
+
+  Widget _buildEmailTextField() {
+    return TextField(
+      controller: emailController,
+      style: const TextStyle(color: Colors.white),
+      decoration: _inputDecoration('Email'),
+    );
+  }
+
+  Widget _buildPasswordTextField() {
+    return TextField(
+      controller: passwordController,
+      style: const TextStyle(color: Colors.white),
+      obscureText: true,
+      decoration: _inputDecoration('Password'),
+    );
+  }
+
+  InputDecoration _inputDecoration(String hintText) {
+    return InputDecoration(
+      hintText: hintText,
+      hintStyle: const TextStyle(color: Color(0xFFC2C2C4)),
+      filled: true,
+      fillColor: Colors.transparent,
+      border: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(7),
+        borderSide: _hasError
+            ? const BorderSide(color: Colors.red, width: 2.0)
+            : const BorderSide(color: Color(0xFF30303D), width: 2.0),
+      ),
+      focusedBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(7),
+        borderSide: const BorderSide(color: Color(0xFFF7DA1B), width: 2.0),
+      ),
+      enabledBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(7),
+        borderSide: _hasError
+            ? const BorderSide(color: Colors.red, width: 2.0)
+            : const BorderSide(color: Color(0xFF30303D), width: 2.0),
+      ),
+      contentPadding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 12.0),
+    );
+  }
+
+  Widget _buildLoginButton() {
+    return SizedBox(
+      width: double.infinity, // Make the button 100% width
+      child: ElevatedButton(
+        onPressed: () async {
+          // Call the login function
+          loginUser();
+        },
+        style: ElevatedButton.styleFrom(
+          backgroundColor: const Color(0xFFF7DA1B),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(7),
+          ),
+          padding: const EdgeInsets.symmetric(vertical: 16.0),
+        ),
+        child: const Text(
+          'Log In',
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
           ),
         ),
       ),
